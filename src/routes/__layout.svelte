d56033a2630648802cc369e1575c50e60f8c32e5
Jonas Kemmer <jo@v2a.net>
2022-01-14 15:26:12 +0100

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import 'virtual:windi.css';
+  import Nav from '../components/nav.svelte';
+</script>
+
+<Nav />
+
+<slot></slot>
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { pokemon } from '../stores/poke.store';
+</script>
+
+{#each $pokemon as pokeman}
+  <p>{pokeman.name}</p>
+{/each}
