8d0b332609717b6639bad9c068b2b33b72d0131f
Jonas Kemmer <jo@v2a.net>
2022-01-14 16:57:16 +0100

    little style update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,8 @@
 	import { pokemon } from '../stores/poke.store';
 </script>
 
+<h1 class="text-4xl text-center my-8 uppercase">Pokedex</h1>
+
 {#each $pokemon as pokeman}
   <p>{pokeman.name}</p>
 {/each}
